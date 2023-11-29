ba5bfca2684d82b3b3e69081c7063cf1ed562da5
dridi alaa <alaadinedridi@gmail.com>
2023-11-29 21:42:04 +0100

    done

---
+++ js/filter-tags.js
@@ -0,0 +1,13 @@
+var array = [ 'Volkswagen', 'BMW', 'Audi', 'Isuzu', 'Lamborghini', 'Ferrari', 'Mercides', 'ShowAll']
+function MyFn(Me) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] != Me.className && Me.className != 'ShowAll') {
+        $("[id="+array[i]+"]").hide();
+      }
+      else {
+        $("[id="+array[i]+"]").show();
+      }
+    }
+}
+
+
